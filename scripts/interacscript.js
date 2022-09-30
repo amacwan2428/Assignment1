3a986814781ae3a865a356275f6891488865a004
amacwan2428 <110256216+amacwan2428@users.noreply.github.com>
2022-09-30 12:25:13 -0400

    changes

--- scripts/interacscript.js
+++ scripts/interacscript.js
@@ -4,6 +4,9 @@ function onsubmitClick(){
     var intamt = document.getElementById('amount').value;
     if(document.getElementById('payeename').value==""){
         document.getElementById('payN').innerHTML = "Enter payee name";
+        payN.style.color = "red";
+        payN.style.fontSize = "12px";
+        payN.style.fontWeight = "bold";
         e1 = 1;
     }else{
         document.getElementById('payN').innerHTML = "";
@@ -12,6 +15,9 @@ function onsubmitClick(){
     if(document.getElementById('payeeemail').value==""){
         document.getElementById('payE').innerHTML = "Enter payee email";
         e2 = 1;
+        payE.style.color = "red";
+        payE.style.fontSize = "12px";
+        payE.style.fontWeight = "bold";
     }else{
         document.getElementById('payE').innerHTML = "";
         e2 = 0;
@@ -19,6 +25,9 @@ function onsubmitClick(){
     if(document.getElementById('payeenum').value==""){
         document.getElementById('payPN').innerHTML = "Enter payee number";
         e3 = 1;
+        payPN.style.color = "red";
+        payPN.style.fontSize = "12px";
+        payPN.style.fontWeight = "bold";
     }else{
         document.getElementById('payPN').innerHTML = "";
         e3 = 0;
@@ -26,6 +35,9 @@ function onsubmitClick(){
     if(document.getElementById('secQ').value==""){
         document.getElementById('paySQ').innerHTML = "Enter security question";
         e4 = 1;
+        paySQ.style.color = "red";
+        paySQ.style.fontSize = "12px";
+        paySQ.style.fontWeight = "bold";
     }else{
         document.getElementById('paySQ').innerHTML = "";
         e4 = 0;
@@ -33,6 +45,9 @@ function onsubmitClick(){
     if(document.getElementById('secA').value==""){
         document.getElementById('paySA').innerHTML = "Enter security answer";
         e5 = 1;
+        paySA.style.color = "red";
+        paySA.style.fontSize = "12px";
+        paySA.style.fontWeight = "bold";
     }else{
         document.getElementById('paySA').innerHTML = "";
         e5 = 0;
@@ -40,6 +55,9 @@ function onsubmitClick(){
     if(document.getElementById('amount').value==""){
         document.getElementById('payAm').innerHTML = "Enter amount";
         e6 = 1;
+        payAm.style.color = "red";
+        payAm.style.fontSize = "12px";
+        payAm.style.fontWeight = "bold";
     }else{
         document.getElementById('payAm').innerHTML = "";
         e6 = 0;
