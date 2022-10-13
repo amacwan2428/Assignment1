6458f645693d1451bd434a5551c76059f65ea912
Luis Brayan <57547348+luisbrayan@users.noreply.github.com>
2022-10-13 16:35:25 -0400

    login message day
    login message day

--- scripts/login.js
+++ scripts/login.js
@@ -1,25 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   var today = new Date().getHours();
-  if (today >= 12 && today <= 18) {
+  if (today >= 12 && today <= 14) {
     document.getElementById("messageLogin").innerHTML = "Good Afternoon"
   } 
-  if (today >=19 && today <= 24) {
+  if (today >=15 || today <= 2) {
     document.getElementById("messageLogin").innerHTML = "Good Evening"
   } 
-  if (today >=1 && today <= 11) {
+  if (today >=3 && today <= 11) {
     document.getElementById("messageLogin").innerHTML = "Good Morning"
   } 
 });
-
-function loginclick(){
-
-  
-  const user = document.getElementById('username');
-  const password = document.getElementById('password'); 
-
-  if (user.value.length === 0 || password.value.length === 0) {
-    document.getElementById("loginResult").innerHTML = "Values Incorrects";
-  } else {
-    window.location.href = "accountSummary.html";
-  }
-}
