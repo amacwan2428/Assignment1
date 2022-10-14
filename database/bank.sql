471601876770f76f3384e1b69b90022382cf5a3d
Luis Brayan <57547348+luisbrayan@users.noreply.github.com>
2022-10-14 18:07:35 -0400

    modify script database
    script database

--- database/bank.sql
+++ database/bank.sql
@@ -11,46 +11,63 @@ CREATE TABLE `client` (
   `email` varchar(250) NOT NULL  
 ) ;
 
-
 CREATE TABLE `account` (
-  `idAccount` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,  
-  `idclient` int(11) NOT NULL,
-    CONSTRAINT FK_client_account FOREIGN KEY (`idclient`)
-    REFERENCES client(`idclient`)
-);
-
-CREATE TABLE `producType` (
-  `idProducType` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `nameProduct` varchar(200) NOT NULL  
-);
-
-CREATE TABLE `accountProduct` (
-  `idAccountProduct` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `idAccount` int(11) NOT NULL,
-  `idProducType` int(11) NOT NULL,
+  `idaccount` int(11) NOT NULL AUTO_INCREMENT  PRIMARY 	KEY,
+  `number` int(11) NOT NULL,
   `balance` decimal(11,2) NOT NULL,
-  CONSTRAINT FK_accountProduct_account FOREIGN KEY (`idAccount`)
-  REFERENCES account(`idAccount`),
-  CONSTRAINT FK_accountProduct_producType FOREIGN KEY (`idProducType`)
-  REFERENCES producType(`idProducType`)  
-);
-
-CREATE TABLE `accountProductTransac` (
-  `idAccountProductTransac` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `idAccountProduct` int(11) NOT NULL,
-  `amount` decimal(11,2) NOT NULL,
-  CONSTRAINT FK_accountPT_accP FOREIGN KEY (`idAccountProduct`)
-  REFERENCES accountProduct(`idAccountProduct`)  
+  `idclient` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `isCreditCard` tinyint(1) NOT NULL,
+  CONSTRAINT FK_client_account FOREIGN KEY (`idclient`)
+    REFERENCES client(`idclient`)
 );
 
 CREATE TABLE `eTransac` (    
   `idETransac` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `idAccountProductTransac` int(11) NOT NULL,
+  `idaccount` int(11) NOT NULL,
   `payeeName` varchar(250) NOT NULL,
   `email` varchar(250) NOT NULL,
   `phoneNumber` varchar(100) NOT NULL,
   `interacQuestion` varchar(250) NOT NULL,
   `interacAnswer` varchar(250) NOT NULL,
-  CONSTRAINT FK_eTransac_accountPT FOREIGN KEY (`idAccountProductTransac`)
-  REFERENCES accountProductTransac(`idAccountProductTransac`)  
+  `amount` decimal(11,2) NOT NULL,
+  CONSTRAINT FK_eTransac_account FOREIGN KEY (`idaccount`)
+  REFERENCES account(`idaccount`)  
 );
+
+
+insert into client values(0,'user1','user1','name user1','150 king ST','647 123456','test@mail.com');
+
+insert into account values(0,123456,5000,1,'savings',0);
+insert into account (idaccount,balance,idclient,iscreditcard,number,type) values(0,350,1,0,151478,'Checking Account');
+
+insert into account (idaccount,balance,idclient,iscreditcard,number,type) values(0,350,1,1,151478,'VISA');
+insert into account (idaccount,balance,idclient,iscreditcard,number,type) values(0,350,1,1,151478,'MASTER CARD');
+
+commit;
+
+DELIMITER $$
+CREATE TRIGGER etransac_trig
+AFTER INSERT ON `etransac` FOR EACH ROW
+begin
+
+       UPDATE account
+       SET balance = balance - NEW.AMOUNT
+       WHERE idaccount = NEW.idaccount;
+       
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER client_trig
+AFTER INSERT ON `client` FOR EACH ROW
+begin
+
+       insert into account
+       values(0,NEW.idclient,0,NEW.idclient,'savings',0);
+       
+       
+END;
+$$
+DELIMITER ;
